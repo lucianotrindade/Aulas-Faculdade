b6b219f635bf078448c5bad5713e62f603fd2592
Luciano <lucianoapatrindade@gmail.com>
2021-09-08 19:49:29 -0300

    Usando modulos no node.js

---
+++ 12_Node_JS/01-Modulos_1/Mod01.js
@@ -0,0 +1,10 @@
+const nome = 'Luciano';
+const sobreNome = 'Trindade';
+
+const falaNome = () =>  nome + ' ' + sobreNome;
+
+
+exports.nome = nome;
+exports.sobreNome = sobreNome;
+exports.falaNome = falaNome;
+
---
+++ 12_Node_JS/01-Modulos_1/app.js
@@ -0,0 +1,3 @@
+const mod1 = require('./Mod01');
+
+console.log(mod1)
