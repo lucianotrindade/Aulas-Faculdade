67340073e56f94a673730b83a80c567a4dcec692
Luciano <lucianoapatrindade@gmail.com>
2021-09-09 21:54:42 -0300

    criando o servidor web básico.

---
+++ 12_Node_JS/06-Servidor_Web_Express/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+
+//          criar    ler    atualizar apagar
+// GRUD --> CREAT,   READ,  UPDATE,   DELETE
+//          POST     GET    PUT       DELETE
+
+app.get('/', (req, res) => {
+    res.send('Ola mundo!'); //podemos colocar um html mas não é recomendavel
+});
+
+// Fazer o servidor ouvir o porta do servidor
+// Estartar o servidor node server.js
+// Ctrl C -> para o servidor
+app.listen(3333, () => {
+    console.log('Acessar http://localhost:3333');
+    console.log('Servidor inicializado!');
+});
+
+// Criando uma outra ROTA
+
+app.get('/contato', (req, res) => {
+    res.send('Obrigado por entrar em contato!');
+});
